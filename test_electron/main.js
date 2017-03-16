ffb057911dc1ac90c6242515d30bc09d8f505e9b
Zixun Xiang <zxiang@fullsail.edu>
2017-03-16 14:50:50 -0400

    added export button

--- test_electron/main.js
+++ test_electron/main.js
@@ -3,38 +3,57 @@ const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
+const ipcRenderer = require('electron').ipcRenderer
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+//let win
 
-function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 1440, height: 780})
+//function createWindow () {
+//    // Create the browser window.
+//    win = new BrowserWindow({width: 1440, height: 780})
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+//    // and load the index.html of the app.
+//    win.loadURL(url.format({
+//        pathname: path.join(__dirname, 'index.html'),
+//        protocol: 'file:',
+//        slashes: true
+//    }))
 
-    // Open the DevTools.
-    win.webContents.openDevTools()
-
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null
-    })
-}
+//    // Emitted when the window is closed.
+//    win.on('closed', () => {
+//        win = null
+//    })
+//}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+//app.on('ready', createWindow)
+app.on('ready', function () {
+    var mainWindow = new BrowserWindow({
+        width: 1440,
+        height: 780
+    })
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html')
+
+    var logWindow = new BrowserWindow({
+        width: 400,
+        height: 400,
+        show: false
+    })
+
+    logWindow.loadURL('file://' + __dirname + '/login.html')
+
+    //ipcRenderer.on('show-prefs', function () {
+    //    logWindow.show()
+    //})
+
+    //ipcRenderer.on('hide-prefs', function () {
+    //    logWindow.hide()
+    //})
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
